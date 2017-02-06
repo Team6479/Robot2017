889f2c95efc90a10576658ab57ca19f18169af02
Jacob Abraham <jacob.r.abraham@gmail.com>
2017-02-05 17:45:39 -0700

    Parsed code out into classes

---
+++ src/org/usfirst/frc/team6479/robot/RacingDrive.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team6479.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+public class RacingDrive extends Teleop
+{
+	// controller object
+	XboxController xbox;
+
+	// get the drivetrain and the port for the controller
+	public RacingDrive(Drivetrain drivetrain, int port)
+	{
+		super(drivetrain);
+		// create controller
+		xbox = new XboxController(port);
+	}
+	// racing drive
+	public void drive()
+	{
+		drivetrain.arcade(rotate(), throttle(), true);
+	}
+	//how much speed
+	public double throttle(){
+		double left = xbox.getRawAxis(2);
+		double right = xbox.getRawAxis(3);
+		//each trigger has an axis range of 0 to 1
+		//to make left trigger reverse, subtract axis value from right trigger
+		return right - left;
+	}
+	//this method is called when the left joystick moves horizontally
+	public double rotate(){
+		double x = xbox.getRawAxis(0);
+		//invert
+		return (x * -1);
+	}
+}
--- src/org/usfirst/frc/team6479/robot/Robot.java
+++ src/org/usfirst/frc/team6479/robot/Robot.java
@@ -24,11 +24,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /* TODO
  * Camera Tracking
- * User inputed scaling for fineDrive()
  * Commands for testing robot testing
  * Test functions
  * Custom Dashboard
  * Misc controls for whatever robot needs to do (i.e. climb rope)
+ * Make custom classes for autonomous
+ * Make custom class for dashboard
  */
 
 /**
@@ -38,50 +39,35 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot {
-	
-	//chooser for auto
+public class Robot extends IterativeRobot
+{
+
+	// the drivetrain
+	Drivetrain drivetrain;
+
+	// chooser for auto
 	SendableChooser<String> autoChooser = new SendableChooser<>();
-	//auto options
+	// auto options
 	final String autoDefault = "default";
 	final String autoOne = "one";
 	final String autoTwo = "two";
-	//which auto is selected
+	// which auto is selected
 	String autoSelected;
-	
-	//chooser for tele
+
+	// chooser for tele
 	SendableChooser<String> teleChooser = new SendableChooser<>();
-	//tele options
-	final String teleDefault = "default";
+	// tele options
 	final String teleArcade = "arcade";
 	final String teleRacing = "racing";
 	final String teleTank = "tank";
-	
-	//which tele is selected
-	String teleSelected;
-	
-	//thread for camera
+
+	// Teleop object, customized in teleopInit
+	Teleop teleopDrive;
+
+	// thread for camera
 	Thread visionThread;
-	//the joysticks
-	//Joystick rightStick = new Joystick(1);
-	//Joystick leftStick = new Joystick(0);
-	
-	//xbox cotroller
-	XboxController xbox = new XboxController(0);
-	
-	//the motor controllers for the drive train
-	Spark leftDrive = new Spark(0);
-	Spark rightDrive = new Spark(1);
-	
-	RobotDrive driveTrain = new RobotDrive(leftDrive, rightDrive);
-	
-	//enums for left and right joystick on
-	public enum JoystickOn {
-		LEFT, RIGHT
-	}
-	//which stick is on
-	JoystickOn stickOn;
-	
+
+	// encoders
 	Encoder leftDriveEncoder;
 	Encoder rightDriveEncoder;
 
@@ -90,26 +76,27 @@ public class Robot extends IterativeRobot {
 	 * used for any initialization code.
 	 */
 	@Override
-	public void robotInit() {
-		GripPipeline pipe = new GripPipeline();
-		
+	public void robotInit()
+	{
+
+		// init drivetrain
+		drivetrain = new Drivetrain(0, 1);
+
 		autoChooser.addDefault("Default Auto", autoDefault);
 		autoChooser.addObject("First Auto", autoOne);
 		autoChooser.addObject("Second Auto", autoTwo);
 		SmartDashboard.putData("Auto Choices", autoChooser);
-		
-		teleChooser.addDefault("Default Tele", teleDefault);
+
 		teleChooser.addObject("Arcade Drive", teleArcade);
 		teleChooser.addObject("Racing Drive", teleRacing);
-		teleChooser.addObject("Tank Drive", teleTank);
+		teleChooser.addDefault("Tank Drive", teleTank);
 		SmartDashboard.putData("Tele Choices", teleChooser);
-		
-		
-		//left drive is inverted since both motors are built identical
-		leftDrive.setInverted(true);
-		
-		//setup camera
-		visionThread = new Thread(() -> {
+
+		GripPipeline pipe = new GripPipeline();
+
+		// setup camera
+		visionThread = new Thread(() ->
+		{
 			// Get the UsbCamera from CameraServer
 			UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
 			// Set the resolution
@@ -124,30 +111,32 @@ public class Robot extends IterativeRobot {
 
 			// Mats are very memory expensive. Lets reuse this Mat.
 			Mat mat = new Mat();
-			
+
 			// This cannot be 'true'. The program will never exit if it is. This
 			// lets the robot stop this thread when restarting robot code or
 			// deploying.
-			while (!Thread.interrupted()) {
+			while(!Thread.interrupted())
+			{
 				// Tell the CvSink to grab a frame from the camera and put it
-				// in the source mat.  If there is an error notify the output.
-				if (cvSink.grabFrame(mat) == 0) {
+				// in the source mat. If there is an error notify the output.
+				if(cvSink.grabFrame(mat) == 0)
+				{
 					// Send the output the error.
 					outputStream.notifyError(cvSink.getError());
 					// skip the rest of the current iteration
 					continue;
 				}
-				
+
 				pipe.process(mat);
 				mat = pipe.maskOutput();
-				
+
 				// Put cross hairs on the image
 				int crossWidth = xRes / 20;
-				System.out.println("xRex" + xRes);
-				System.out.println("width of line" + crossWidth);
 				int crossHeight = yRes / 20;
-				Imgproc.line(mat, new Point((xRes / 2) - (crossWidth / 2), (yRes / 2)), new Point((xRes / 2) + (crossWidth / 2), (yRes / 2)), new Scalar(0, 255, 0), 5);
-				Imgproc.line(mat, new Point((xRes / 2), (yRes / 2) + (crossHeight / 2)), new Point((xRes / 2), (yRes / 2) - (crossHeight / 2)), new Scalar(0, 255, 0), 5);
+				Imgproc.line(mat, new Point((xRes / 2) - (crossWidth / 2), (yRes / 2)),
+						new Point((xRes / 2) + (crossWidth / 2), (yRes / 2)), new Scalar(0, 255, 0), 5);
+				Imgproc.line(mat, new Point((xRes / 2), (yRes / 2) + (crossHeight / 2)),
+						new Point((xRes / 2), (yRes / 2) - (crossHeight / 2)), new Scalar(0, 255, 0), 5);
 				// Give the output stream a new image to display
 				outputStream.putFrame(mat);
 			}
@@ -168,15 +157,17 @@ public class Robot extends IterativeRobot {
 	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	@Override
-	public void autonomousInit() {
-		//autoSelected = autoChooser.getSelected();
-		//get the selected autonomous
+	public void autonomousInit()
+	{
+		// autoSelected = autoChooser.getSelected();
+		// get the selected autonomous
 		autoSelected = autoChooser.getSelected();
-		
-		//init the encoders
+
+		// init the encoders
 		leftDriveEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
 		rightDriveEncoder = new Encoder(3, 4, false, Encoder.EncodingType.k4X);
-		//set the time until when the robot is considered stopped, set in seconds
+		// set the time until when the robot is considered stopped, set in
+		// seconds
 		leftDriveEncoder.setMaxPeriod(.05);
 		rightDriveEncoder.setMaxPeriod(.05);
 	}
@@ -185,8 +176,10 @@ public class Robot extends IterativeRobot {
 	 * This function is called periodically during autonomous
 	 */
 	@Override
-	public void autonomousPeriodic() {
-		switch (autoSelected) {
+	public void autonomousPeriodic()
+	{
+		switch(autoSelected)
+		{
 		case autoOne:
 			// Put custom auto code here
 			break;
@@ -196,8 +189,8 @@ public class Robot extends IterativeRobot {
 		case autoDefault:
 		default:
 			// Put default auto code here
-			
-			//basic autonomous, drive forward, then turn left
+
+			// basic autonomous, drive forward, then turn left
 			break;
 		}
 	}
@@ -206,155 +199,57 @@ public class Robot extends IterativeRobot {
 	 * This function is called at the start of operator control
 	 */
 	@Override
-	public void teleopInit() {
-		//get the teleop driving config
-		teleSelected = teleChooser.getSelected();
+	public void teleopInit()
+	{
+		// get the teleop driving config
+		String teleSelected = teleChooser.getSelected();
 		SmartDashboard.putString("Selected", teleSelected);
-		//if its for arcade, set right stick on
-		if(teleSelected.equals(teleArcade)) {
-			stickOn = JoystickOn.RIGHT;
+
+		// choose which teleop is selected
+		switch(teleSelected)
+		{
+		case teleArcade:
+			teleopDrive = new ArcadeDrive(drivetrain, 0);
+			break;
+		case teleRacing:
+			teleopDrive = new RacingDrive(drivetrain, 0);
+			break;
+		case teleTank:
+		default:
+			teleopDrive = new TankDrive(drivetrain, 0);
+			break;
 		}
-		//init the encoders
+
+		// init the encoders
 		leftDriveEncoder = new Encoder(0, 1, false, Encoder.EncodingType.k4X);
 		rightDriveEncoder = new Encoder(3, 4, true, Encoder.EncodingType.k4X);
-		//set the time until when the robot is considered stopped, set in seconds
+		// set the time until when the robot is considered stopped, set in
+		// seconds
 		leftDriveEncoder.setMaxPeriod(.05);
 		rightDriveEncoder.setMaxPeriod(.05);
 	}
-	
+
 	/**
 	 * This function is called periodically during operator control
 	 */
 	@Override
-	public void teleopPeriodic() {
-		
+	public void teleopPeriodic()
+	{
+
 		SmartDashboard.putNumber("Left Distance", leftDriveEncoder.getDistance());
 		SmartDashboard.putNumber("Right Distance", rightDriveEncoder.getDistance());
 		SmartDashboard.putBoolean("Left Direction", leftDriveEncoder.getDirection());
 		SmartDashboard.putBoolean("Right Direction", rightDriveEncoder.getDirection());
-		
-		//choose which teleop is selected
-		switch (teleSelected) {
-		case teleArcade:
-			//call the arcade function
-			arcade();
-			break;
-		case teleRacing:
-			//call function for racing drive
-			racing();
-			break;
-		case teleTank:
-			//calculates left side, sets left drive speed to y axis of xbox left
-			leftDrive.set(xbox.getY(Hand.kLeft) );
-			//calculates right side, sets right drive speed to y axis of xbox right
-				rightDrive.set(xbox.getY(Hand.kRight) );
-			break;
-		case teleDefault:
-		default:
-			//This will be tank drive
-			break;
-		}
-		
-	}
-	
-	public void racing(){
-		driveTrain.arcadeDrive(rotate(), throttle());
-	}
-	//arcade drive
-	public void arcade() {
-		//if the right stick is on and the leftstick is clicked
-		//switch which stick is on to left stick
-		if(stickOn == JoystickOn.RIGHT && xbox.getStickButton(Hand.kLeft))
-		{
-			stickOn = JoystickOn.LEFT;
-		}
-		//if the left stick is on and the rightstick is clicked
-		//switch which stick is on to right stick
-		if(stickOn == JoystickOn.LEFT && xbox.getStickButton(Hand.kRight))
-		{
-			stickOn = JoystickOn.RIGHT;
-		}
-		
-		//if the right stick is on, use full drive
-		if(stickOn == JoystickOn.RIGHT)
-		{
-			fullDrive();
-		}
-		//if the left stick is on, use fine drive
-		if(stickOn == JoystickOn.LEFT)
-		{
-			fineDrive();
-		}
-	}
-	//determine if joystick is being moved
-	//param is which joystick on controller
-	public double throttle(){
-		double left = xbox.getRawAxis(2);
-		double right = xbox.getRawAxis(3);
-		//each trigger has an axis range of 0 to 1
-		//to make left trigger reverse, subtract axis value from right trigger
-		return right - left;
-	}
-	//this method is called when the left joystick moves horizontally
-	public double rotate(){
-		double x = xbox.getRawAxis(0);
-		//invert
-		return (x * -1);
-	}
-	public boolean isJoystickMoving(Hand hand) {
-		//get x and y
-		double x = xbox.getX(hand);
-		double y = xbox.getY(hand);
-		
-		//is the value of x within .001 of zero
-		boolean isXZero = Math.abs(x) <= .01;
-		
-		//is the value of y within .001 of zero
-		boolean isYZero = Math.abs(y) <= .01;
-		
-		//if x and y are zero, joystick is not moving, return false
-		if(isXZero && isYZero) {
-			return false;
-		}
-		else {
-			return true;
-		}
-	}
-	
-	//for driving
-	//this function is called when the right joystick on the xbox cotroller is being used
-	//gives full control, going from -1 to 1
-	public void fullDrive() {
-		
-		//get x and y
-		double x = xbox.getRawAxis(4);
-		double y = xbox.getRawAxis(5);
-		
-		//invert
-		driveTrain.arcadeDrive(x * -1, y * -1, true);
-	}
-	
-	//for driving
-	//this function is called when the left joystick on the xbox cotroller is being used
-	//gives fine control, going from -.5 to .5
-	public void fineDrive() {
-		
-		//get x and y
-		double x = xbox.getRawAxis(0);
-		double y = xbox.getRawAxis(1);
-		
-		//scale the speed
-		//eventually this will get scaling information from driver station or controller, for now just scale to 1/10
-		double scaleFactor = .5;
-		
-		//invert and scale
-		driveTrain.arcadeDrive(x * scaleFactor * -1, y * scaleFactor * -1, false);
+
+		teleopDrive.drive();
+
 	}
 	/**
 	 * This function is called periodically during test mode
 	 */
 	@Override
-	public void testPeriodic() {
-		
+	public void testPeriodic()
+	{
+
 	}
 }
